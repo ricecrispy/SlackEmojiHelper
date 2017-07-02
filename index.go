d07ce011c136f1086742bc954494b66ffa4458ab
Jeffrey Siu <jeffreysiu@Jeffreys-MacBook-Pro.local>
2017-07-01 22:10:36 -0400

    Added spam and InsertClaps commands

---
+++ index.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintln(w, "Command - spam: /spam/{emoji}/{num of emoji}")
+	fmt.Fprintln(w, "Command - insert claps emojis between each word: /insertclaps/{sentence}")
+	fmt.Fprintln(w, "Skrt Skrt.")
+}
+
+func InsertClaps(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	input := ps.ByName("input")
+	inputArr := strings.Split(input, " ")
+
+	fmt.Fprintln(w, len(inputArr))
+
+	for i := 0; i < len(inputArr); i++ {
+		word := inputArr[i]
+		inputArr[i] = word + " :clap:"
+	}
+
+	output := strings.Join(inputArr, " ")
+	fmt.Fprintln(w, output)
+}
+
+func Spam(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	output := ""
+	emoji := ps.ByName("emoji")
+	num, err := strconv.Atoi(ps.ByName("num"))
+	if err != nil {
+		fmt.Fprintln(w, err)
+	} else {
+		for i := 0; i < num; i++ {
+			output += emoji
+		}
+		fmt.Fprintln(w, output)
+	}
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/insertclaps/:input", InsertClaps)
+	router.GET("/spam/:emoji/:num", Spam)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
