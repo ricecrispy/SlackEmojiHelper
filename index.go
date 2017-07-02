fcc527858dee6cdea2a259cee83899da50d56741
Jeffrey Siu <jeffreysiu@Jeffreys-MBP.fios-router.home>
2017-07-02 00:01:59 -0400

    Changed host to Github

--- index.go
+++ index.go
@@ -51,5 +51,5 @@ func main() {
 	router.GET("/insertclaps/:input", InsertClaps)
 	router.GET("/spam/:emoji/:num", Spam)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe("https://ricecrispy.github.io/SlackEmojiHelper/", router))
 }
