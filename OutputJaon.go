28ff2ee66e6de75a9f6242076394d0168152dd7a
Jeffrey Siu <jeffrey.p.siu@gmail.com>
2017-07-03 14:15:30 -0400

    Changed Json key name

--- OutputJaon.go
+++ OutputJaon.go
@@ -1,18 +1,6 @@
 package main
 
 type OutputJson struct {
-	Output string
+	Text string `json:"text"`
 }
 
-type SlackPostRequest struct {
-	Token string `json:"token"`
-	Team_id string `json:"team_id"`
-	Team_domain string `json:"team_domain"`
-	Channel_id string `json:"channel_id"`
-	Channel_name string `json:"channel_name"`
-	User_id string `json:"user_id"`
-	User_name string `json:"user_name"`
-	Command string `json:"command"`
-	Text string `json:"text"`
-	Response_url string `json:"response_url"`
-}
