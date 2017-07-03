9ea677de41ad79f59f21b5416050517bc7786bce
Jeffrey Siu <jeffrey.p.siu@gmail.com>
2017-07-03 11:16:07 -0400

    Added SlackPostRequest struct

--- OutputJaon.go
+++ OutputJaon.go
@@ -3,3 +3,16 @@ package main
 type OutputJson struct {
 	Output string
 }
+
+type SlackPostRequest struct {
+	Token string `json:"token"`
+	Team_id string `json:"team_id"`
+	Team_domain string `json:"team_domain"`
+	Channel_id string `json:"channel_id"`
+	Channel_name string `json:"channel_name"`
+	User_id string `json:"user_id"`
+	User_name string `json:"user_name"`
+	Command string `json:"command"`
+	Text string `json:"text"`
+	Response_url string `json:"response_url"`
+}
